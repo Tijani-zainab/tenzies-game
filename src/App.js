d37b02307be0d0766c49b4cb06861d749dac9071
Tijani Zainab <jani.tijj@gmail.com>
2022-07-26 23:26:13 +0100

    dice css

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
 import React from "react";
 import './App.css';
+import Die from "./component/Die/Die";
 
 function App() {
   return (
-    <main className="main-container">
-        <div className="container"></div>
+    <main className="main">
+        <div className="dice-container ">
+            <Die value="1" />
+            <Die value="2" />
+            <Die value="3" />
+            <Die value="4" />
+            <Die value="5" />
+            <Die value="6" />
+            <Die value="1" />
+            <Die value="1" />
+            <Die value="1" />
+            <Die value="1" />
+        </div>
     </main>
   );
 }
---
+++ src/component/Die/Die.js
@@ -0,0 +1,12 @@
+import React from "react";
+import './../../App.css';
+
+function Die(props) {
+  return (
+    <div className="die-face">
+        <h2 className="die-num">{props.value}</h2>
+    </div>
+  );
+}
+
+export default Die;
