34e7ba816ad3948c95790d55c1da791da90c4450
Tijani Zainab <jani.tijj@gmail.com>
2022-08-05 00:24:32 +0100

    get random numbers

--- src/App.js
+++ src/App.js
@@ -3,6 +3,21 @@ import './App.css';
 import Die from "./component/Die/Die";
 
 function App() {
+
+  //   function allNewDice(max) { 
+  //     return Math.floor(Math.random() * max);
+  // }
+
+  // console.log(allNewDice(7));
+
+  let arr = [];
+  for (let   i=0, t=6; i<t; i++) {
+    arr.push(Math.round(Math.random() * t))
+  }
+  console.log(arr);
+
+ // Array.from({length: 40}, () => Math.floor(Math.random() * 40));
+
   return (
     <main className="main">
         <div className="dice-container ">
