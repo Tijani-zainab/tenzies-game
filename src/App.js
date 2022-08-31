38263c16d142c86e984a65c47c94b7165a849274
Zainab Tijani <jani.tijj@gmail.com>
2022-09-01 00:16:11 +0100

    add: hold die and conditional styling

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { useState } from "react";
+import { nanoid } from "nanoid";
 import './App.css';
 import Die from "./component/Die/Die";
 
@@ -10,21 +11,43 @@ function App() {
   function allNewDice() { // this function will generate a new array of random numbers
     let arrNum = []
     for (let i = 0, c = 10; i < c; i++) {
-      arrNum.push(Math.round(Math.random() * 6))
+      arrNum.push(
+        {
+          value: Math.round(Math.random() * 6),
+          isHeld: false,
+          id: nanoid()
+        })
     }
     return arrNum;
   }
 
-  const rollDice = dice.map(die => <Die value={die} />); // this function will map the array of random numbers to the dice component
+  const rollDice = dice.map(die => <Die
+    holdDie={() => holdDie(die.id)}
+    isHeld={die.isHeld}
+    key={die.id}
+    value={die.value} />); // this function will map the array of random numbers to the dice component
+
+  const roll = () => { setDice(allNewDice()); }
+
+  function holdDie(id) {
+    setDice(dice.map(die => {
+      if (die.id === id) {
+        die.isHeld = !die.isHeld;
+      }
+      return die;
+    }));
+
+    console.log(id);
+  }
 
   return (
     <main className="main">
       <h1 className="logo">Tenzies</h1>
       <h2>Dice Roller</h2>
-      <div className="dice-container ">
+      <div className="dice-container">
         {rollDice}
       </div>
-      <button className="btn" onClick={() => setDice(allNewDice())}>Roll Dice</button>
+      <button className="btn" onClick={roll}>Roll Dice</button>
     </main>
   );
 }
--- src/component/Die/Die.js
+++ src/component/Die/Die.js
@@ -2,8 +2,13 @@ import React from "react";
 import "./../../App.css";
 
 function Die(props) {
+
+  const styles = {
+    backgroundColor: props.isHeld ? "#59E391" : "none",
+  }
+
   return (
-    <div className="die-face">
+    <div className="die-face" style={styles} onClick={props.holdDie}>
       <h2 className="die-num">{props.value}</h2>
     </div>
   );
